c7ed836ff5ce5d82c10c5fbf2a15126f74c514d4
Mahmoud Rekapi <m.rekapi@gmail.com>
2020-08-09 13:09:05 +0200

    the first commit

---
+++ PS01.py
@@ -0,0 +1,40 @@
+# 1. How to find average N numbers in python
+# 2. How to sum of the first N positive integers in Python
+# 3. How to get time of a python program's execution
+# 4. How to get the current user name in python
+# 5. How to access environment variables
+# 6. How to do a profile a python script
+# 7. How to list all files of a directory
+# 8. find out the version of python you are using
+# 9. print current time and date
+# 10. find area of a circle
+# 11. reverse a string
+# 12. create a list and tuple with comma separated numbers
+# 13. extract extension from file name
+# 14. display the first and last colors from a given list in python
+# 15. display a sample examination schedule
+# 16. how to read a number b and compute n+nn+nnn
+# 17. how to print the documents of python built in function
+# 18. how to print a calendar for the given month and year
+# 19. how to write string without having escape
+# 20. how to calculate number of days between two dates
+# 21. how to find volume and surface area of sphere
+# 22. how to find the difference between two numbers
+# 23. calculate sum of three numbers if the values are equal then return thrice of their sum
+# 24. how to get a new string from a given string
+# 25. how to find whether an integer is even or odd number
+# 26. how to reverse a number using slice operation
+# 27. how to find the biggest and smallest of 3 numbers using list
+# 28. calculate the sum of elements in a list
+# 29. how to find the area of triangle
+# 30. how to find the area of triangle given base and height
+# 31. how to get a string which is n copies of a given string
+# 32. how to count the number of items in a list
+# 33. find n copies of the first 2 characters of a given string
+# 34. how to check if item is in list or not
+# 35. how to create histogram from a given list of integers
+# 36. how to turn a list into a string
+# 37. how to print even numbers in the given range
+# 38. how to compare lists
+# 39. how to find the greatest common divisor GCD in python
+# 40. how to find least common multiple LCM
---
+++ PS02.py
@@ -0,0 +1,31 @@
+# Continue
+# 41. how to sum two given numbers and return a number (functions)
+# 42. how to add two objects if both objects are an integer type
+# 43. how to display name, age, address in three different lines
+# 44. how to solve (x + y) * (x + Y)
+# 45. how to calculate the future value of rate of interest and a number of years
+# 46. how to calculate the distance between two points
+# 47. check if a file exists
+# 48. check if python version is 64 or 32
+# 49. check the os name, platform and release information
+# 50. how to find the location of python site package directory
+# 51. how to call an external command
+# 52. how to get path and name of a file that currently executing in python
+# 53. how to find out the number of CPUs
+# 54. how to convert strings to float or int
+# 55. how to print stderr
+# 56. how to sort counter by value
+# 57. converts height in feet and inches to centimeters
+# 58. calculate the hypotenuse of a right angled triangle
+# 59. how to get file creation & modification date times
+# 60. calculate body mass index
+# 61. converts seconds to day, hour, minutes and seconds
+# 62. get a list of built in modules
+# 63. how to find ASCII value of a character
+# 64. remove the first item of a list
+# 65. how to read the contents of an URL
+# 66. how to get the system hostname
+# 67. how to swap two variables
+# 68. how to concatenate N strings
+# 69. how to find file path or directory
+# 70. how to get the users environment (77 in the PlayList)
