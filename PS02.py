cfa4d72063a43c688d7bc46f4ee7ebfb88f63439
mahmoud.mohamed <m.rekapi@gmail.com>
2020-08-16 01:44:26 +0200

    finishing until 77 in the playlist begin new file

--- PS02.py
+++ PS02.py
@@ -1,5 +1,9 @@
 # Continue
+from __future__ import print_function
+import sys
 import math
+import textwrap
+import http.client
 
 
 # 41. how to Sum two given numbers and return a number (functions)
@@ -43,15 +47,20 @@ def solve_eq(a, b, c):
     return x1 / z, x2 / z
 
 
-a1 = float(input("The value of a : "))
+"""a1 = float(input("The value of a : "))
 b1 = float(input("The value of b : "))
-c1 = float(input("The value of c : "))
+c1 = float(input("The value of c : "))"""
 
-print("(x1, x2) =", solve_eq(a1, b1, c1))
+print("(x1, x2) =", solve_eq(1, 5, 6))
 
 # 45. how to calculate the future value of rate of interest and a number of years
 
 # 46. how to calculate the distance between two points
+p1 = [4, 0]
+p2 = [6, 8]
+distance = math.sqrt(((p1[0] - p2[0]) ** 2 + ((p1[1] - p2[1]) ** 2)))
+print(distance)
+
 # 47. check if a file exists
 # 48. check if python version is 64 or 32
 # 49. check the os name, platform and release information
@@ -60,17 +69,41 @@ print("(x1, x2) =", solve_eq(a1, b1, c1))
 # 52. how to get path and name of a file that currently executing in python
 # 53. how to find out the number of CPUs
 # 54. how to convert strings to float or int
+n = "244.3"
+print(float(n))
+print(int(float(n)))
+
+
 # 55. how to print stderr
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+eprint("abc", "efg", "xyz", sep="__")
+
 # 56. how to sort counter by value
+
 # 57. converts height in feet and inches to centimeters
+
 # 58. calculate the hypotenuse of a right angled triangle
 # 59. how to get file creation & modification date times
 # 60. calculate body mass index
 # 61. converts seconds to day, hour, minutes and seconds
+
 # 62. get a list of built in modules
+module_name = ', '.join(sorted(sys.builtin_module_names))
+print(textwrap.fill(module_name, width=70))
+
 # 63. how to find ASCII value of a character
 # 64. remove the first item of a list
+
 # 65. how to read the contents of an URL
+conn = http.client.HTTPConnection("www.google.com")
+conn.request("GET", "/")
+result = conn.getresponse()
+contents = result.read()
+print(contents)
+
 # 66. how to get the system hostname
 # 67. how to swap two variables
 # 68. how to concatenate N strings
